65c236f55219039af262c25acd14e0491e4adafe
Alex Arditti <alex.arditti@hotmail.com>
2020-10-08 12:10:44 +0100

    #4 apiSetUp
    added quiz and timer component to quiz.js 
    added QuestionBox and Results Component

--- frontend/src/component/Quiz.js
+++ frontend/src/component/Quiz.js
@@ -1,41 +1,84 @@
-import React from 'react';
-import './css/Styles.css';
+import React, { Component, useState } from 'react';
+import axios from 'axios';
+import QuestionBox from "./QuestionBox";
+import Results from "./Results";
 
 const Quiz = () => {
 
     //* ALEX - THINK YOUR QUIZ API CODE SHOULD GO HERE
     //! CODE TO PULL QUESTIONS FROM API AND DISPLAY BELOW FOR QUIZ PAGE
 
-    return (
-        <div className="quizDiv">
+class Quiz extends Component {
+  constructor(props) {
+    super(props)
+    this.state =  {
+      questionBank: [],
+      score: 0,
+      responses: 0,
+      count: 0
+    };
+  }
 
-        <h2>Question 1</h2>
-        <h3>What is your name</h3>
-        <h2>A - B - C - D</h2>
-        <h3>--------------</h3>
-        
-        <h2>Question 2</h2>
+   getApi = async () => {
+    const res =  await axios.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple");
+    console.log(res.data.results); 
+    this.setState({
+      questionBank: res.data.results
+    })
+
+  }
         <h3>What is your quest</h3>
         <h2>A - B - C - D</h2>
         <h3>--------------</h3>
-        
-        <h2>Question 3</h2>
-        <h3>What is your favourite colour</h3>
-        <h2>A - B - C - D</h2>
-        <h3>--------------</h3>
-        
-        <h2>Question 4</h2>
-        <h3>What is the average air velocity of an African Swallow</h3>
-        <h2>A - B - C - D</h2>
-        <h3>--------------</h3>
-        
-        <h2>Question 5</h2>
-        <h3>What is your name</h3>
-        <h2>A - B - C - D</h2>
-        <h3>--------------</h3>
 
-        </div>
-    )
+  computeAnswer = (incorrect_answers, correct_answer) => {
+    if( incorrect_answers ===  correct_answer) {
+      this.setState({
+        score: this.state.score + 1
+      })
+    }
+    this.setState({
+      responses: this.state.responses < 10 ? this.state.responses +1 : 10
+    })
+  }
+
+  componentDidMount() {
+    this.getApi()
+    this.myInterval = setInterval(() =>{
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000)
+
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+      
+          <h1>timer {count}</h1>
+      
+        <div className="title">Quiz</div>
+        {this.state.questionBank.length > 0 && 
+        this.state.responses < 10 &&
+          this.state.questionBank.map(
+            ({question, correct_answer, incorrect_answers}) => 
+            <QuestionBox 
+              question={question} 
+              options={incorrect_answers}
+              correct ={correct_answer}
+              key={question} 
+              selected={incorrect_answers => this.computeAnswer(incorrect_answers, correct_answer)}
+            />
+          )}
+
+          {this.state.responses === 10 ? (<Results score={this.state.score} />)
+          : null}
+          
+      </div>
+
+    );
+  }
 }
-
 export default Quiz;
