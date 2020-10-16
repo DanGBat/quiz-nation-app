df899b1b9e35b022c0131b0d94f6266b1924544b
Danny GIllespie <danny.gillespie@hotmail.com>
2020-10-16 14:12:45 +0100

    Last Modifications

--- frontend/src/component/HomeText.js
+++ frontend/src/component/HomeText.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import profilePic from "../images/quizBanner.png";
 import './css/Styles.css';
 import './css/Home.css';
 
@@ -8,31 +10,23 @@ const HomeText = () => {
         <div className="homeTextDiv">
 
             <h1>Welcome to Quiz Nation</h1>
-            <h2>Lorem</h2>
+            <Link to="/"><img src={profilePic} alt="Profile" className="Profile" /></Link>
+            <h2>Everyone Enjoys A Quiz</h2>
             <p>
-                Nulla ad non elit eiusmod cupidatat ex eiusmod est occaecat officia incididunt labore labore.
-                Irure dolor voluptate est est consectetur culpa sit. Adipisicing duis tempor reprehenderit
-                excepteur commodo. Excepteur et eu culpa ut consectetur fugiat esse sint sunt enim amet ea
-                sint commodo. Aute ipsum aliqua non mollit ipsum exercitation adipisicing ut. Lorem qui nulla 
-                occaecat cillum.
+                Please feel free to take one of our quizzes or just browse our Quiz App.
+                Here at QuizNation we don't want to limit any users from having the ability to enjoy
+                playing all our wonderfull quizzes.
+                However, we do suggest, for a more taylored and enjoyable experience you sign up
+                to be a part of the great QuizNation community.
             </p>
 
-            <h2>Lorem</h2>
+            <h2>Logged in Users</h2>
             <p>
-                Nulla ad non elit eiusmod cupidatat ex eiusmod est occaecat officia incididunt labore labore.
-                Irure dolor voluptate est est consectetur culpa sit. Adipisicing duis tempor reprehenderit
-                excepteur commodo. Excepteur et eu culpa ut consectetur fugiat esse sint sunt enim amet ea
-                sint commodo. Aute ipsum aliqua non mollit ipsum exercitation adipisicing ut. Lorem qui nulla 
-                occaecat cillum.
-            </p>
-
-            <h2>Lorem</h2>
-            <p>
-                Nulla ad non elit eiusmod cupidatat ex eiusmod est occaecat officia incididunt labore labore.
-                Irure dolor voluptate est est consectetur culpa sit. Adipisicing duis tempor reprehenderit
-                excepteur commodo. Excepteur et eu culpa ut consectetur fugiat esse sint sunt enim amet ea
-                sint commodo. Aute ipsum aliqua non mollit ipsum exercitation adipisicing ut. Lorem qui nulla 
-                occaecat cillum.
+                Being logged in gives you the abiliy to create a bespoke profile and connect
+                with like minded quizzers from around the world.
+                It also means that your quiz scores will be automatically subbmitted to our
+                global scoreboard where you can see how well you are matched against all
+                the other quiz loving users from the QuizNation community.
             </p>
 
         </div>
